c8dec83f6b51ebbb5cdcab8a0d9d848691bd68e1
jgoode7 <joegoode.07@gmail.com>
2026-01-30 08:12:32 -0800

    Add category list to products page

--- everyday-market-app/src/app/market/products-page/products-page.ts
+++ everyday-market-app/src/app/market/products-page/products-page.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { Category } from '../models/category';
+import { CategoryMenu } from '../category-menu/category-menu';
 
 @Component({
   selector: 'app-products-page',
-  imports: [],
+  standalone: true,
+  imports: [CategoryMenu],
   templateUrl: './products-page.html',
   styleUrl: './products-page.css',
 })
 export class ProductsPage {
-
+  categoryList: Category[] = [
+    { id: 1, name: 'Gaming' },
+    { id: 2, name: 'Cars' },
+    { id: 3, name: 'Photography' },
+    { id: 4, name: 'True Crime' },
+    { id: 5, name: 'Travel' },
+  ];
 }
