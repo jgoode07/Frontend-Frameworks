36457f2a71678e1b20f4fd85faa01ae9298596c2
jgoode7 <joegoode.07@gmail.com>
2026-01-30 08:35:26 -0800

    Connect category menu to display categories

--- everyday-market-app/src/app/market/category-menu/category-menu.ts
+++ everyday-market-app/src/app/market/category-menu/category-menu.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Category } from '../models/category';
+import { CategoryMenuItem } from '../category-menu-item/category-menu-item';
 
 @Component({
   selector: 'app-category-menu',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, CategoryMenuItem],
   templateUrl: './category-menu.html',
   styleUrl: './category-menu.css',
 })
 export class CategoryMenu {
+  @Input() categories: Category[] = [];
+  @Output() categorySelected = new EventEmitter<Category>();
 
+  onItemClicked(category: Category) {
+    this.categorySelected.emit(category);
+  }
 }
