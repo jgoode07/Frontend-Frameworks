260a94dcae8b46083e7210afd300a87cad0d942a
jgoode7 <joegoode.07@gmail.com>
2026-01-30 15:36:20 -0800

    Style header and add brand layout
    Placeholder.co for now, replace with images later

--- everyday-market-app/src/app/market/category-menu-item/category-menu-item.ts
+++ everyday-market-app/src/app/market/category-menu-item/category-menu-item.ts
@@ -8,7 +8,11 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   styleUrl: './category-menu-item.css',
 })
 export class CategoryMenuItem {
+
+  // Name of the category to display on the button
   @Input() categoryName = '';
+
+  // Emits when the item is clicked
   @Output() itemClicked = new EventEmitter<string>();
 
   onItemClick() {
