c95e0c9aaaa7c6eb5fc10cd8065f1266108fb06d
jgoode7 <joegoode.07@gmail.com>
2026-01-30 08:47:23 -0800

    Add input and click event to category menu item

--- everyday-market-app/src/app/market/category-menu-item/category-menu-item.ts
+++ everyday-market-app/src/app/market/category-menu-item/category-menu-item.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-category-menu-item',
+  standalone: true,
   imports: [],
   templateUrl: './category-menu-item.html',
   styleUrl: './category-menu-item.css',
 })
 export class CategoryMenuItem {
+  @Input() categoryName = '';
+  @Output() itemClicked = new EventEmitter<string>();
 
+  onItemClick() {
+    this.itemClicked.emit(this.categoryName);
+  }
 }
