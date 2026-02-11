65bb8264dd72b8b64ed387a5140cf1e9401ab71a
jgoode7 <joegoode.07@gmail.com>
2026-02-11 09:41:03 -0800

    Create register reactive form group

--- Angular/everyday-market-app/src/app/shared/register-page/register-page.ts
+++ Angular/everyday-market-app/src/app/shared/register-page/register-page.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register-page',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './register-page.html',
   styleUrl: './register-page.css',
 })
 export class RegisterPage {
+  private fb = inject(FormBuilder);
 
+  // Main registration form structure
+  registrationForm = this.fb.group({
+    name: [''],
+    email: [''],
+    phone: [''],
+    dateOfBirth: [''],
+    streetAddress: [''],
+    province: [''],
+    country: ['Canada'],
+    acceptTerms: [false],
+  });
 }
